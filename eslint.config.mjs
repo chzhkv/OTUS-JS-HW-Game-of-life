22728c410fe57146baeb902c35fbfe0d4f9abdfd
chzhkv <chizhikvo@gmail.com>
2024-08-04 19:11:50 +0400

    add github actions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,27 +5,25 @@ import tseslint from "typescript-eslint";
 import jest from "eslint-plugin-jest";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-
 export default tseslint.config(
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    {
-      languageOptions: { globals: globals.browser }
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+  },
+  {
+    ignores: ["coverage/*", "dist/*", "**/*.config.js"],
+  },
+  {
+    files: ["**/*.test.ts"],
+    ...jest.configs["flat/recommended"],
+    rules: {
+      ...jest.configs["flat/recommended"].rules,
+      "jest/prefer-expect-assertions": "off",
     },
-    {
-      files: ["**/*.{js,mjs,cjs,ts}"]
-    },
-    {
-      ignores: ["coverage/*", "dist/*", "**/*.config.js"],
-    },
-    {
-        files: ["**/*.test.ts"],
-        ...jest.configs["flat/recommended"],
-        rules: {
-            ...jest.configs["flat/recommended"].rules,
-            "jest/prefer-expect-assertions": "off",
-        },
-    },
-    eslintConfigPrettier,
+  },
+  eslintConfigPrettier,
 );
-
